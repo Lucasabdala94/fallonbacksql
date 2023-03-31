4c56ab5fed0360d2e23b90971e5d07d9c8be48c2
lucas <lucasabdala94@gmail.com>
2023-03-31 03:57:40 -0300

    agregado midelware

--- config/middlewares.js
+++ config/middlewares.js
@@ -15,8 +15,8 @@ module.exports = [
         useDefaults: true,
         directives: {
           'connect-src': ["'self'", 'https:'],
-          'img-src': ["'self'", 'data:', 'blob:', 'dl.airtable.com', 'res.cloudinary.com','https://res.cloudinary.com/dgjk5awtd','https://fallon-back.onrender.com/api'],
-          'media-src': ["'self'", 'data:', 'blob:', 'dl.airtable.com', 'res.cloudinary.com','https://fallon-back.onrender.com/api'],
+          'img-src': ["'self'", 'data:', 'blob:', 'dl.airtable.com', 'res.cloudinary.com','https://res.cloudinary.com/dgjk5awtd','https://fallon-backsql2.onrender.com/api'],
+          'media-src': ["'self'", 'data:', 'blob:', 'dl.airtable.com', 'res.cloudinary.com','https://fallon-backsql2.onrender.com/api'],
           upgradeInsecureRequests: null,
         },
       },
