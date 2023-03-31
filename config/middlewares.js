c81ad785d5cd0900b13d14701e050b87a5083ed3
lucas <lucasabdala94@gmail.com>
2023-03-31 01:38:32 -0300

    first commit

---
+++ config/middlewares.js
@@ -0,0 +1,25 @@
+module.exports = [
+  'strapi::errors',
+  'strapi::cors',
+  'strapi::poweredBy',
+  'strapi::logger',
+  'strapi::query',
+  'strapi::body',
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': ["'self'", 'data:', 'blob:', 'dl.airtable.com', 'res.cloudinary.com','https://res.cloudinary.com/dgjk5awtd','https://fallon-back.onrender.com/api'],
+          'media-src': ["'self'", 'data:', 'blob:', 'dl.airtable.com', 'res.cloudinary.com','https://fallon-back.onrender.com/api'],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+];
